3197267efe6df629c8c03d44bff3f5c6813c32d0
larissa_santana <larissa.santana@napista.com.br>
2020-08-08 11:26:54 -0300

    criando a classe DataContext

---
+++ Shop/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shop.Models;
+
+namespace Shop.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        // DbSet = representação das tabelas. Então se eu tenho um DbSet<Product>,
+        // uma tabela chamada Product vai ser buscada no banco e o mapeamento com
+        // o modelo Product vai ser feito.
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
