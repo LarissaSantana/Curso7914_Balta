9c1dce51b169557438ce9dc0d4ab8378ed0881b1
larissa_santana <larissa.santana@napista.com.br>
2020-08-08 17:05:32 -0300

    criando o tokenService

---
+++ Shop/Controllers/UserController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Controllers
+{
+    [Route("v1/users")]
+    public class UserController : ControllerBase
+    {
+
+    }
+}
