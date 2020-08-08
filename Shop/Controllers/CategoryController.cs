476e2b763a74db6d92f72452bc6a4ffa460c240c
larissa_santana <larissa.santana@napista.com.br>
2020-08-07 23:38:45 -0300

    criando CategoryController

---
+++ Shop/Controllers/CategoryController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Controllers
+{
+    [Route("categories")]
+    public class CategoryController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public ActionResult GetById(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public ActionResult Post()
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public ActionResult Put()
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public ActionResult Delete()
+        {
+            return Ok();
+        }
+    }
+}
