380d0400e6920f45ace3f68eb1054b95644f46cc
larissa_santana <larissa.santana@napista.com.br>
2020-08-08 12:53:10 -0300

    Criando o ProductController

---
+++ Shop/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shop.Data;
+using Shop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Controllers
+{
+    [Route("products")]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> Get([FromServices] DataContext context)
+        {
+            var produtcs = await context.Products
+                .Include(p => p.Category)
+                .AsNoTracking()
+                .ToListAsync();
+
+            return Ok(produtcs);
+        }
+    }
+}
